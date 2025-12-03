147a897fefc5d7a03b04cded55410a87ff2d8cda
Ubuntu <azureuser@imamura-ima-learn.5ufp0hy2n2zunlwlgctifv2h1g.lx.internal.cloudapp.net>
2025-12-03 09:43:11 +0000

    chore: clarify hash algo display

--- pcr10-calc/main.py
+++ pcr10-calc/main.py
@@ -1,8 +1,10 @@
 import calculate_pcr10
+import hashlib
 
 # Path to IMA log
 IMA_LOG_PATH = '/sys/kernel/security/ima/ascii_runtime_measurements'
 
 if __name__ == "__main__":
-    pcr_value = calculate_pcr10.get_pcr10_sha256(IMA_LOG_PATH)
-    print(f"PCR 10 value: {pcr_value.hex().upper()}")
+    pcr_value_sha256 = calculate_pcr10.get_pcr10_sha256(IMA_LOG_PATH)
+    print(f"PCR 10 value (SHA-256): {pcr_value_sha256.hex().upper()}")
+
