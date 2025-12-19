c4dafa0affe620f2704b15daf68e518c1be2be04
Takuma IMAMURA <209989118+hyperfinitism@users.noreply.github.com>
2025-12-19 15:13:43 +0900

    feat: move IMA log entry parser to classmethod

--- examples/pcr10.py
+++ examples/pcr10.py
@@ -2,16 +2,11 @@
 Example script for calculating PCR10 from IMA log file.
 """
 import hashlib
-import sys
-import os
-
-# Add parent directory to path to import from py package
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 from imapcr10 import read_ima_log_file, calculate_pcr10
 
 DEFAULT_IMA_LOG_PATH = "/sys/kernel/security/ima/ascii_runtime_measurements"
 
-def set_hash_function(hash_type: str):
+def select_hash_function(hash_type: str):
     match hash_type:
         case "sha1":
             return hashlib.sha1
@@ -30,8 +25,8 @@ def main():
     print(f"Using IMA log path: {input_path}")
     pcr_hash_type = input("Enter the hash function to use for the PCR10 (default: sha256): ") or "sha256"
 
-    # Set hash function for PCR10 hash chain
-    pcr_hash_func = set_hash_function(pcr_hash_type)
+    # Select hash function for PCR10 hash chain
+    pcr_hash_func = select_hash_function(pcr_hash_type)
 
     # Read IMA log entries
     entries = read_ima_log_file(input_path)
